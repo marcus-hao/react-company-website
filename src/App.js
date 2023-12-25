ff5e90c4e818fe61b4af8d59d419bf550b8a8dd2
Marcus Chan <24224821+marcus-hao@users.noreply.github.com>
2023-12-25 17:02:38 +0800

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import AboutUs from './components/AboutUs'; // Import the AboutUs component
+import NavBar from './components/NavBar/NavBar';
 import { useEffect } from 'react';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <h1>HabisLiao Solutions</h1>
       </header>
       <main>
+        <NavBar /> {/* Include the NavBar component */}
         <AboutUs /> {/* Include the AboutUs component */}
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </main>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from '../../assets/images/logo-white-transparent.png';
+
+const NavBar = () => {
+    return (
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center justify-between w-full">
+                        <div className="flex-shrink-0">
+                            <img className="h-12 w-32" src={Logo} alt="Logo" />
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="ml-10 flex items-baseline space-x-4">
+                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
+                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
